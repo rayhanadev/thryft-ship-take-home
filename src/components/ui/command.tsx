2cfde4a0396672b818bc52a4bd82980e16a96eab
ray <me@rayhanadev.com>
2024-07-18 01:43:41 -0500

    chore: lint files

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -1,12 +1,13 @@
 "use client"
 
 import * as React from "react"
+
 import { type DialogProps } from "@radix-ui/react-dialog"
 import { MagnifyingGlassIcon } from "@radix-ui/react-icons"
 import { Command as CommandPrimitive } from "cmdk"
 
-import { cn } from "~/lib/utils"
 import { Dialog, DialogContent } from "~/components/ui/dialog"
+import { cn } from "~/lib/utils"
 
 const Command = React.forwardRef<
   React.ElementRef<typeof CommandPrimitive>,
@@ -23,7 +24,7 @@ const Command = React.forwardRef<
 ))
 Command.displayName = CommandPrimitive.displayName
 
-interface CommandDialogProps extends DialogProps {}
+type CommandDialogProps = DialogProps
 
 const CommandDialog = ({ children, ...props }: CommandDialogProps) => {
   return (
